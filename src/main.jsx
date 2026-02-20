6c2dc758a6bbd65c899f09ab9381c2d935a67695
Latte114 <latte1332011@gmail.com>
2026-02-20 21:01:43 +0700

    Initial commit: Science Quiz app

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../Science Quiz.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
